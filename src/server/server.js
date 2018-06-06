ced772649c4c94847fe2c8db6b980a73471e9acd
Jingyu Yao <yjy.emails@gmail.com>
2018-06-06 17:25:21 -0400

    added json body parser

--- src/server/server.js
+++ src/server/server.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 const express = require('express');
+const bodyParser = require('body-parser');
 const path = require('path');
 const mongoose = require('mongoose');
 
@@ -8,6 +9,7 @@ mongoose.connect(process.env.MONGODB_URI);
 
 const app = express();
 
+app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, '..', '..', 'dist/webdev-angular')));
 
 app.listen(process.env.PORT || 8080);

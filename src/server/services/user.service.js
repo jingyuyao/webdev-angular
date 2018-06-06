416a4f842829265905872d9501b74a5dfaad728d
Jingyu Yao <yjy.emails@gmail.com>
2018-06-06 18:55:43 -0400

    login, logout, delete user

--- src/server/services/user.service.js
+++ src/server/services/user.service.js
@@ -2,17 +2,64 @@ const UserModel = require('../models/user.model');
 
 function addUserService(app) {
   app.post('/api/user', createUser);
+  app.delete('/api/user/:userId', deleteUser);
+  app.post('/api/login', login);
+  app.post('/api/logout', logout);
+
+  // debug
   app.get('/api/user', findAllUsers);
 }
 
 function createUser(req, res) {
-  const user = new UserModel(req.body);
-  user.save(err => {
+  UserModel.create(req.body, (err, user) => {
     if (err) {
       res.status(400).json({error: err.message});
     } else {
       res.json(user);
     }
+  });
+}
+
+function deleteUser(req, res) {
+  const userId = req.params.userId;
+  if (req.session.userId === userId) {
+    UserModel.deleteOne({_id: userId}, err => {
+      if (err) {
+        res.status(400).json({error: err.message});
+      } else {
+        res.json({})
+      }
+    });
+  } else {
+    res.status(401).json({error: 'Unauthorized'});
+  }
+}
+
+function login(req, res) {
+  if (req.body && req.body.username && req.body.password) {
+    UserModel
+      .findOne({
+        username: req.body.username,
+        password: req.body.password,
+      })
+      .exec((err, user) => {
+        if (err) {
+          res.status(400).json({error: err.message});
+        } else if (!user) {
+          res.status(404).json({error: 'Not found'});
+        } else {
+          req.session.userId = user._id;
+          res.json(user);
+        }
+      });
+  } else {
+    res.status(400).json({error: 'Missing params'});
+  }
+}
+
+function logout(req, res) {
+  req.session.destroy(() => {
+    res.json({});
   });
 }
 
@@ -22,7 +69,7 @@ function findAllUsers(req, res) {
     .select({password: 0})
     .exec((err, users) => {
       if (err) {
-        res.status(400).json({error: errmessage});
+        res.status(400).json({error: err.message});
       } else {
         res.json(users);
       }

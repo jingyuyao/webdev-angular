d87a2a71d242c9ab9ff62fd6e6f0fab848b610e3
Jingyu Yao <yjy.emails@gmail.com>
2018-06-07 15:56:50 -0400

    crud section services

--- src/server/services/section.service.js
+++ src/server/services/section.service.js
@@ -5,7 +5,7 @@ function addSectionService(app) {
   app.get('/api/section/:sectionId', findSection);
   app.put('/api/section/:sectionId', updateSection);
   app.delete('/api/section/:sectionId', removeSection);
-  app.get('/api/course/:courseId/section', findCourseSections);
+  app.get('/api/course/:courseId/sections', findCourseSections);
 
   // debug
   app.get('/api/section', findAllSections);
@@ -22,15 +22,55 @@ function createSection(req, res) {
 }
 
 function findSection(req, res) {
+  SectionModel
+    .findById(req.params.sectionId)
+    .exec((err, section) => {
+      if (err) {
+        res.status(400).json({error: err.message});
+      } else if (!section) {
+        res.status(404).json({error: 'Not found'});
+      } else {
+        res.json(section);
+      }
+    });
 }
 
 function updateSection(req, res) {
+  SectionModel
+    .findByIdAndUpdate(req.params.sectionId, req.body, {new: true})
+    .exec((err, section) => {
+      if (err) {
+        res.status(400).json({error: err.message});
+      } else if (!section) {
+        res.status(404).json({error: 'Not found'});
+      } else {
+        res.json(section);
+      }
+    });
 }
 
 function removeSection(req, res) {
+  SectionModel
+    .findByIdAndDelete(req.params.sectionId)
+    .exec(err => {
+      if (err) {
+        res.status(400).json({error: err.message});
+      } else {
+        res.json({})
+      }
+    });
 }
 
 function findCourseSections(req, res) {
+  SectionModel
+    .find({courseId: req.params.courseId})
+    .exec((err, sections) => {
+      if (err) {
+        res.status(400).json({error: err.message});
+      } else {
+        res.json(sections);
+      }
+    });
 }
 
 function findAllSections(req, res) {

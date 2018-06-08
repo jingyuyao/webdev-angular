12044221bd130e2ca76a2400ac6fc349ece5da56
Jingyu Yao <yjy.emails@gmail.com>
2018-06-08 15:14:06 -0400

    added config service

---
+++ src/app/services/config.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { environment } from '../../environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ConfigService {
+
+  constructor() { }
+
+  getStudentApiUrl(path: string): string {
+    return environment.studentApiUrl + path;
+  }
+
+  getTeacherApiUrl(path: string): string {
+    return 'https://jingyuyao-webdev-1.herokuapp.com';
+  }
+}
